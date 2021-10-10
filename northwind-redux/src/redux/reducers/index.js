db31bd5895400fd54862e2a81ed85bfa28555e03
ipekguzel8 <ipekgzel8@gmail.com>
2021-10-10 03:01:29 +0300

    first commit

---
+++ northwind-redux/src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import changeCategoryReducer from "./changeCategoryReducers"
+const rootReducer= combineReducers({
+    changeCategoryReducer
+})
+export default rootReducer;
